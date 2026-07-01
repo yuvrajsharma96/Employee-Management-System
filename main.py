59136e81f0c9fa528c585a94298d613bef4effee
Yuvraj Sharma <yuvrajsharma9685@gmail.com>
2026-07-01 09:53:53 +0530

    adding main menu

---
+++ main.py
@@ -0,0 +1,14 @@
+from employee import Employee
+import database
+
+
+def menu():
+    print("\n -------Employee Management System---------")
+    print("1. to add employee")
+    print("2. to view employee")
+    print("3. to search employee")
+    print("4. to delet employee")
+    print("5. Exit")
+
+
+
