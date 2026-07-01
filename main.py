b25ed4d86189d26c242fc766c27af87835f8c6a3
Yuvraj Sharma <yuvrajsharma9685@gmail.com>
2026-07-01 10:10:27 +0530

    making menu functions working

--- main.py
+++ main.py
@@ -12,3 +12,33 @@ def menu():
 
 
 
+while True :
+    menu()
+
+    choice = input("enter your choice")
+
+    if choice == "1":
+        emp_id = input("enter employee ID: ")
+        name = input("enter employee name: ")
+        age = input ("enter employee age: ")
+        department = input ("enter employee department: ")
+        salary = input ("enter employee salary: ")
+        
+        emp = Employee(emp_id,name,age,department,salary)
+
+        database.add_employee(emp.to_dict())
+
+        print ("employee added sucessfully")
+
+    elif choice == "2":
+        employees = database.view_employee()
+
+        if not employees :
+            print("no employee found ")
+        else: 
+            for emp in employees:
+             print("-------------")
+             for key , value in emp.items():
+                 print(f"{key}: {value}")
+
+
