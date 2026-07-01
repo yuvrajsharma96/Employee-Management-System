fc53d772591cf832582395926f7aa692c66b512f
Yuvraj Sharma <yuvrajsharma9685@gmail.com>
2026-07-01 09:47:37 +0530

    adding delete employee

--- database.py
+++ database.py
@@ -11,15 +11,24 @@ def load_data():
             return json.load(f)
         except:
             return[]
+
+
 def save_data(data):
     with open(File,"w") as f:
         json.dump(data,f,indent=4)
+
+
 def add_employee(employee):
     data = load_data()
     data.append(employee)
     save_data(data)
+
+
+
 def view_employee():
     return load_data()
+
+
 
 def search_employee(emp_id):
     data = load_data()
@@ -27,5 +36,14 @@ def search_employee(emp_id):
         if emp["ID"] == emp_id:
             return emp
         return None
+    
+
+
+def delete_employee(emp_id):
+    data = load_data()
+    new_data =[emp for emp in data if emp["ID"] != emp_id]
+    save_data(new_data)
+
+
 
 
